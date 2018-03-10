e4bbc9376ac3bd0b7734653e160c58c2a1f10339
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-03-10 09:01:37 +0900

    update tests

--- tests/test-http2-server-push.php
+++ tests/test-http2-server-push.php
@@ -5,8 +5,8 @@ class Http2_Server_Push_Test extends WP_UnitTestCase
 	public function test_get_enqueued_items()
 	{
 		add_action( 'wp_enqueue_scripts', function() {
-			wp_enqueue_style( 'test', 'test.css' );
-			wp_enqueue_script( 'test', 'test.js' );
+			wp_enqueue_style( 'test', 'test.css', false, 123 );
+			wp_enqueue_script( 'test', 'test.js', false, 456 );
 		} );
 
 		do_action( 'wp_enqueue_scripts' );
@@ -18,7 +18,7 @@ class Http2_Server_Push_Test extends WP_UnitTestCase
 		$this->assertTrue( is_array( $urls['script'] ) );
 		$this->assertSame( 1, count( $urls['style'] ) );
 		$this->assertSame( 1, count( $urls['script'] ) );
-		$this->assertSame( "test.css?ver=5.0-alpha-42808", $urls['style'][0] );
-		$this->assertSame( "test.js?ver=5.0-alpha-42808", $urls['script'][0] );
+		$this->assertSame( "test.css?ver=123", $urls['style'][0] );
+		$this->assertSame( "test.js?ver=456", $urls['script'][0] );
 	}
 }
